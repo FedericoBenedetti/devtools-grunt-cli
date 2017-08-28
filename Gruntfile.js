33c2e93ffbe3b0c90b06c0803b8a14d58821d71b
Classe Virtuale <classev@loccioni.com>
2017-08-28 14:30:46 +0200

    grunt: missed parenthesis

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-﻿/*
+﻿﻿/*
 This file in the main entry point for defining grunt tasks and using grunt plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
@@ -17,5 +17,5 @@ module.exports = function (grunt) {
                 jshintrc: '.jshintrc'
             }
         }
-    }
+    });
 };
