03c5278dc553c752b089b1ccdc2550ef35a9f914
Classe Virtuale <classev@loccioni.com>
2017-08-28 17:42:27 +0200

    added the capability to compile while writing
    * need typescript package for this, so add it to package.json
    * added tsconfig.json to define how to compile our code
    * added local template (from: https://git.loccioni.com/IT/gui-project-template)
    * updated gitignore

---
+++ tasks/gl-grunt-template-creator.js
@@ -0,0 +1,5 @@
+module.exports = (grunt) => {
+    grunt.registerMultiTask('unzip_template', 'Unzip the local template', function () {
+    });
+};
+//# sourceMappingURL=gl-grunt-template-creator.js.map
---
+++ tasks/gl-grunt-template-creator.ts
@@ -0,0 +1,4 @@
+﻿module.exports = (grunt: IGrunt) => {
+    grunt.registerMultiTask('unzip_template', 'Unzip the local template', function() {
+    }        
+}
