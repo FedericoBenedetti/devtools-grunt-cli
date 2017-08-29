beffac718f310839e2e1ab568c3274c8da4a4cbc
Classe Virtuale <classev@loccioni.com>
2017-08-29 14:23:22 +0200

    refactoryze code following grunt-init

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,26 @@
+'use strict';
+
 module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
 
-    grunt.loadNpmTasks('grunt-zip');
-    grunt.loadNpmTasks('grunt-contrib-rename');
-
-    grunt.task.loadTasks('tasks');
-
     grunt.initConfig({
-        pkg: grunt.file.readJSON('./package.json'),
         jshint: {
             all: [
                 'Gruntfile.js',
-                'tasks/*.js'
+                'tasks/*.js',
+                'bin/gl-cli'
             ],
             options: {
                 jshintrc: '.jshintrc'
             }
-        },
-
-        unzip: {
-            'out': 'templates/gui-template.zip'
-        },
-
-        rename: {
-            main: {
-                files: [
-                    { src: ['out/gui-project-template.sln'], dest: 'out/' + grunt.option("title") + '.sln' },
-                ]
-            }
-        },
+        }
 
     });
 
-    grunt.registerTask('default', ['jshint', 'unzip', 'rename', 'modify_template']);
+    grunt.task.loadTasks('tasks');
+
+    grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks('grunt-contrib-rename');
+
+    grunt.registerTask('default', ['jshint']);
 };
