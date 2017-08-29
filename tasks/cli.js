7974318104598b35fcc9549417c28172636249d5
Classe Virtuale <classev@loccioni.com>
2017-08-29 14:48:25 +0200

    formatted and rivisited some code
    * to use gl-cli:
    * cd bin/
    * node gl-cli

--- tasks/cli.js
+++ tasks/cli.js
@@ -1,30 +1,17 @@
-/*
- * grunt-init
- * https://gruntjs.com/
- *
- * Copyright (c) 2016 "Cowboy" Ben Alman, contributors
- * Licensed under the MIT license.
- */
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Nodejs libs.
   var path = require('path');
 
-  // The "init" task needs separate delimiters to avoid conflicts, so the <>
-  // are replaced with {}. Otherwise, they behave the same.
   grunt.template.addDelimiters('init', '{%', '%}');
 
-  // ==========================================================================
-  // TASKS
-  // ==========================================================================
-
-  grunt.registerInitTask('init', 'Generate project scaffolding from a template.', function() {
-    // Extra arguments will be applied to the template file.
+  grunt.registerInitTask('init', 'Generate project scaffolding from a template.', function () {
     var args = grunt.util.toArray(arguments);
-    console.log("gl-cli aquired");
+
+    console.log("args: " + args);
   });
 
 };
