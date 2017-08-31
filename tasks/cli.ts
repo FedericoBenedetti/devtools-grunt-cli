e3020e693f9b40da9d8813ffdc5c153ca2716be4
Classe Virtuale <classev@loccioni.com>
2017-08-31 11:34:02 +0200

    move logic to gl-cli

--- tasks/cli.ts
+++ tasks/cli.ts
@@ -66,54 +66,12 @@ module.exports = (grunt: IGrunt) => {
         _map.set("web", new TemplateWeb);
         _map.set("desktop", new TemplateDesktop);
 
-        if (args.length < 4) {
-            if (args[2] == "help" || args[2] == "h") {
-                grunt.help.queue.forEach(helpMsg => {
-                    grunt.help[helpMsg]();
-                });
-                process.exit();
-
-            } else if (args[2] != "web" ||
-                args[2] != "desktop") {
-
-                grunt.log.writeln("\n LOG: 1 argument detected, using it as a title. ");
-                setTitle(args[2]);
-
-                grunt.log.writeln("\n LOG: using 'web' as default template. ")
-                executeTasks("web");
-
-            } else if (!args[2]) {
-
-                grunt.log.writeln("\n LOG: No args detected, using 'web' as default template. ");
-                executeTasks("web");
-            }
-
-        } else if (args.length == 4) {
-            setTitle(args[3]);
-            executeTasks(args[2]);
-        } else if (args.length > 4) {
-            if (args[2] == "verbose" || args[2] == "v") {
-                grunt.option("verbose", true);
-
-                grunt.log.writeln("\n LOG: verbose detected. ");
-
-                setTitle(args[4]);
-                executeTasks(args[3]);
-            }
-        }
-
+        executeTasks();
     });
 
-    function setTitle(title: string) {
-        grunt.log.writeln("\n LOG: template name: " + "'" + title + "'");
-        grunt.option("pjtitle", title);
-    }
+    function executeTasks() {
 
-    function executeTasks(type: string) {
-        grunt.log.writeln("\n LOG: type of task: '" + type + "'");
-
-        _taskToExecute = _map.get(type).execute();
-        grunt.log.writeln("\n LOG: command that is being executed: " + _taskToExecute);
+        _taskToExecute = _map.get(grunt.option("type")).execute();
 
         grunt.task.run(_taskToExecute);
     }
